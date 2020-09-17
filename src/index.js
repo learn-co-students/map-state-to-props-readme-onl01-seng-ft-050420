fb3b3b2077c8d2e484b573ddc19c35918a1d6c19
codewithirene567 <irenejinscott@gmail.com>
2020-09-16 21:28:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -9,8 +10,13 @@ const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
+//provider coming from the react-redux library is doing two things
+//alerting that there has been a change in the state and this will rerender our react app
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store = {store}>
+    
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
+//Line 16 with the provider is making it available to all our other components
